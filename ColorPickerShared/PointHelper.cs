d632a09df83c843ff409a528b9e2e0d645e183df
Wouter de Vries <wouterdevries@silverfit.nl>
2023-08-12 17:20:46 +0200

    Move to PointHelper

---
+++ ColorPickerShared/PointHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.Foundation;
+
+namespace ColorPickerShared;
+
+public static class PointHelper
+{
+    /// <summary>
+    /// Calculates the absolute distance between two points
+    /// </summary>
+    /// <param name="p1">The first point</param>
+    /// <param name="p2">The second point</param>
+    /// <returns>The absolute distance between the points</returns>
+    public static double Distance(Point p1, Point p2)
+    {
+        return Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+    }
+}
--- ColorPickerUwp/Views/ColorGroupView.xaml.cs
+++ ColorPickerUwp/Views/ColorGroupView.xaml.cs
@@ -102,17 +102,6 @@ namespace ColorPickerUwp.Views
             return closestContainerIndex;
         }
 
-        /// <summary>
-        /// Calculates the absolute distance between two points
-        /// </summary>
-        /// <param name="p1">The first point</param>
-        /// <param name="p2">The second point</param>
-        /// <returns>The absolute distance between the points</returns>
-        private double Distance(Point p1, Point p2)
-        {
-            return Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
-        }
-
         private void NamePressed(object sender, PointerRoutedEventArgs e)
         {
             this.nameEdit.Visibility = Visibility.Visible;
